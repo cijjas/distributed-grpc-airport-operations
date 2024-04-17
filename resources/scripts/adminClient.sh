c0352d2fb368544112281fa776925f1be54c2fda
cijjas <cijjas@itba.edu.ar>
2024-04-16 21:39:56 -0300

    Change proto response definitions for admin service

---
+++ resources/scripts/adminClient.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CLIENT_JARS="../../client/target/tpe1-g7-client-2024.1Q/lib/jars/*"
+TARGET_CLIENT="ar.edu.itba.pod.tpe1.client.admin.AdminClient"
+
+
+tar -xzf "../../client/target/tpe1-g7-client-2024.1Q-bin.tar.gz" -C "../../client/target/"
+java -cp "$CLIENT_JARS"  "$TARGET_CLIENT" "$@"
+
