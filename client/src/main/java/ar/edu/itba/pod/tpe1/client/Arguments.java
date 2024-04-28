02346b2e688edb73cb1593f5dc8ba86b3a6aff18
TobiasPerry <tperry@itba.edu.ar>
2024-04-28 19:16:36 -0300

    catched parse exception

--- client/src/main/java/ar/edu/itba/pod/tpe1/client/Arguments.java
+++ client/src/main/java/ar/edu/itba/pod/tpe1/client/Arguments.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.pod.tpe1.client;
 
+import org.apache.commons.cli.ParseException;
+
 public class Arguments {
     private String ip = "localhost";
     private Integer port = 0;
@@ -11,27 +13,28 @@ public class Arguments {
         return port;
     }
 
-    public void parseServerAddress(String serverAddress) {
-        String[] parts = serverAddress.split(":");
-        if (parts.length != 2) {
-            throw new IllegalArgumentException("Invalid server address");
-        }
-        //make regex to match localhost and or ip address
-        if (!parts[0].matches("^(localhost|\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})$")) {
-            throw new IllegalArgumentException("Invalid IP address");
-        }
-        if (!parts[1].matches("^[0-9]+$")) {
-            throw new IllegalArgumentException("Invalid port");
-        }
+    public void parseServerAddress(String serverAddress) throws ParseException {
+            String[] parts = serverAddress.split(":");
+            if (parts.length != 2) {
+                throw new ParseException("Invalid server address");
+            }
+            //make regex to match localhost and or ip address
+            if (!parts[0].matches("^(localhost|\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})$")) {
+                throw new ParseException("Invalid IP address");
+            }
+            if (!parts[1].matches("^[0-9]+$")) {
+                throw new ParseException("Invalid port");
+            }
 
-        this.ip = parts[0];
-        this.port = Integer.parseInt(parts[1]);
+            this.ip = parts[0];
+            this.port = Integer.parseInt(parts[1]);
+
     }
 
     public String getServerAddress() {
         return ip + ":" + port;
     }
-    public void setServerAddress(String serverAddress) {
+    public void setServerAddress(String serverAddress) throws ParseException {
         parseServerAddress(serverAddress);
     }
 }
